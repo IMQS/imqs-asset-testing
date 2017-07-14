c1b89cdea29c683966dca689ca0c2599e3f48d2d
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2017-07-14 15:01:33 +0200

    Corrected bad table deletes

--- scripts/v6/stored-procs/01-delete-stored-procs.sql
+++ scripts/v6/stored-procs/01-delete-stored-procs.sql
@@ -1,6 +1,6 @@
 IF OBJECT_ID ('CreateSCOABatch') IS NOT NULL DROP PROCEDURE CreateSCOABatch;
 IF OBJECT_ID ('ExportSamrasSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportSamrasSCOAJournalRollUp;
-IF OBJECT_ID ('ExportSamrasSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportSamrasSCOAJournalRollUp;
+IF OBJECT_ID ('ExportSamrasSCOAMasterDataForBatch') IS NOT NULL DROP PROCEDURE ExportSamrasSCOAMasterDataForBatch;
 IF OBJECT_ID ('ExportSCOAJournalNoRollup') IS NOT NULL DROP PROCEDURE ExportSCOAJournalNoRollup;
 IF OBJECT_ID ('ExportSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportSCOAJournalRollUp;
 IF OBJECT_ID ('ExportSolarSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportSolarSCOAJournalRollUp;
