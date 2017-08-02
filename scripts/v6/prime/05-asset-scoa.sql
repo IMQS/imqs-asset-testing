b0f2e2ff4b380a7928291d4215f039a328f980d7
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2017-08-02 17:43:45 +0200

    Deleted old priming scripts

--- scripts/v6/prime/05-asset-scoa.sql
+++
@@ -1,9 +0,0 @@
--- Insert SCOADepreciationStatus stuff
-insert into SCOADepreciationStatus ([SCOAJournalID], [Status], [Information]) values (1, 4, 'Peter Pan is pending Auth');
-insert into SCOADepreciationStatus ([SCOAJournalID], [Status], [Information]) values (2, 4, 'Peter Pan is pending Auth');
-insert into SCOADepreciationStatus ([SCOAJournalID], [Status], [Information]) values (3, 4, 'Peter Pan is pending Auth');
-insert into SCOADepreciationStatus ([SCOAJournalID], [Status], [Information]) values (4, 4, 'Peter Pan is pending Auth');
-insert into SCOADepreciationStatus ([SCOAJournalID], [Status], [Information]) values (5, 4, 'Peter Pan is pending Auth');
-insert into SCOADepreciationStatus ([SCOAJournalID], [Status], [Information]) values (6, 4, 'Peter Pan is pending Auth');
-insert into SCOADepreciationStatus ([SCOAJournalID], [Status], [Information]) values (7, 4, 'Peter Pan is pending Auth');
-insert into SCOADepreciationStatus ([SCOAJournalID], [Status], [Information]) values (8, 4, 'Peter Pan is pending Auth');
