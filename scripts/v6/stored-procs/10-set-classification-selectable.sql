fbf95684a71a5a024b6fb9fe7c7523a3664cf2f1
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2017-08-28 14:43:16 +0200

    Added stored proc for setting breakdown tree selectable

---
+++ scripts/v6/stored-procs/10-set-classification-selectable.sql
@@ -0,0 +1,43 @@
+IF OBJECT_ID (N'SetClassificationSelectable', N'P') IS NOT NULL DROP PROCEDURE [SetClassificationSelectable];
+CREATE PROCEDURE [dbo].[SetClassificationSelectable]
+		@accountNumber VARCHAR(50)
+AS
+	BEGIN
+		WITH [CTE] (SCOAId, ParentSCOAId, SCOAFile, SCOAAccount, ShortDescription, SCOALevel, AccountNumber, bPostingLevel, PostingLevelDescription, AccountNumberPrefix, VATStatus, BreakDownAllowed, DefinitionDescription, IsSelectable)
+		AS (
+			SELECT
+				c.SCOAId, c.ParentSCOAId, c.SCOAFile, c.SCOAAccount, c.ShortDescription, c.SCOALevel, c.AccountNumber, c.bPostingLevel, c.PostingLevelDescription, c.AccountNumberPrefix, c.VATStatus, c.BreakDownAllowed, c.DefinitionDescription, c.IsSelectable
+			FROM
+				SCOAClassification c
+			WHERE
+				AccountNumber = @accountNumber
+			UNION ALL
+			SELECT
+				c.SCOAId,
+				c.ParentSCOAId,
+				c.SCOAFile,
+				c.SCOAAccount,
+				c.ShortDescription,
+				c.SCOALevel,
+				c.AccountNumber,
+				c.bPostingLevel,
+				c.PostingLevelDescription,
+				c.AccountNumberPrefix,
+				c.VATStatus,
+				c.BreakDownAllowed,
+				c.DefinitionDescription,
+				c.IsSelectable
+			FROM
+				[CTE] p, SCOAClassification c
+			WHERE
+				c.SCOAId = p.ParentSCOAId
+		)
+		UPDATE
+			SCOAClassification
+		SET
+			IsSelectable = 1
+		FROM
+			CTE
+		WHERE
+			SCOAClassification.SCOAId = CTE.SCOAId
+	END;
