aa2f767c4f5c399b9772e90ee80d94bc0f3274b9
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2017-08-30 16:33:49 +0200

    Joined CTE with underlying source table in UPDATE clauses

--- scripts/v6/stored-procs/10-set-classification-selectable.sql
+++ scripts/v6/stored-procs/10-set-classification-selectable.sql
@@ -2,9 +2,6 @@ CREATE PROCEDURE [dbo].[SetClassificationSelectable]
 	@accountNumber VARCHAR(50)
 AS
 BEGIN
-
-	DECLARE @selectables TABLE (SCOAId NVARCHAR(250), ParentSCOAId NVARCHAR(250), SCOAFile NVARCHAR(100), SCOAAccount NVARCHAR(1024), ShortDescription NVARCHAR(500), SCOALevel INT, AccountNumber NVARCHAR(250), bPostingLevel NVARCHAR(50), PostingLevelDescription NVARCHAR(250), BreakDownAllowed NVARCHAR(50), IsSelectable BIT, IsBreakdown BIT);
-
 	WITH CTE
 	AS (
 		SELECT
@@ -38,35 +35,26 @@ BEGIN
 			c.SCOAId = p.ParentSCOAId
 	)
 
-	INSERT INTO @selectables SELECT
-		cte.SCOAId,
-		cte.ParentSCOAId,
-		IIF(CTE.IsBreakdown = 1, (select t.SCOAFile from cte t where t.SCOAId = cte.ParentSCOAId), CTE.SCOAFile),
-		(select t.SCOAAccount from cte t where t.SCOAId = CTE.ParentSCOAId) + ':' + CTE.ShortDescription,
-		cte.ShortDescription,
-		IIF(CTE.IsBreakdown = 1, (select t.SCOALevel+1 from cte t where t.SCOAId = cte.ParentSCOAId), CTE.SCOALevel),
-		cte.AccountNumber,
-		1,
-		cte.PostingLevelDescription,
-		'No',
-		1,
-		cte.IsBreakdown
+	UPDATE
+		SCOAClassification
+	SET
+		IsSelectable = 1,
+		SCOAAccount = (select t.SCOAAccount from cte t where t.SCOAId = CTE.ParentSCOAId) + ':' + CTE.ShortDescription,
+		SCOALevel = IIF(CTE.IsBreakdown = 1, (select t.SCOALevel+1 from cte t where t.SCOAId = cte.ParentSCOAId), CTE.SCOALevel),
+		SCOAFile = IIF(CTE.IsBreakdown = 1, (select t.SCOAFile from cte t where t.SCOAId = cte.ParentSCOAId), CTE.SCOAFile),
+		bPostingLevel = 1,
+		BreakDownAllowed = 'No'
 	FROM
-		cte
+		CTE
+		INNER JOIN
+		SCOAClassification sc ON sc.AccountNumber = CTE.AccountNumber
 	WHERE
-		CTE.AccountNumber = @accountNumber AND 'Yes' = (select t.BreakDownAllowed from cte t where SCOAId = CTE.ParentSCOAId);
+		CTE.AccountNumber = @accountNumber;
 
 	UPDATE
 		SCOAClassification
 	SET
-		IsSelectable = s.IsSelectable,
-		SCOAAccount = s.SCOAAccount,
-		SCOALevel = s.SCOALevel,
-		SCOAFile = s.SCOAFile,
-		bPostingLevel = s.bPostingLevel,
-		BreakDownAllowed = s.BreakDownAllowed
-	FROM
-		@selectables s
+		bPostingLevel = 0
 	WHERE
-		s.SCOAAccount = @accountNumber;
+		SCOAId = (select ParentSCOAId from SCOAClassification where AccountNumber = @accountNumber);
 END;
