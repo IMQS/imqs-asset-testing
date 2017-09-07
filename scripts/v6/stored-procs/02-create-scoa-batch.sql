d5d857c58b51eccc14f0863d4e4f7ce6fe711e2f
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2017-09-07 14:33:26 +0200

    Fixed batch create script

--- scripts/v6/stored-procs/02-create-scoa-batch.sql
+++ scripts/v6/stored-procs/02-create-scoa-batch.sql
@@ -28,9 +28,9 @@ BEGIN
 
 	-- From the batch rows in the SCOAJournal, we create a @rollups virtual table, and allocate each
 	-- batch row with a rollup id (using the rank() windowed function), grouped across each separate rollup value
-	DECLARE @hasDates BIT = case @fromDate when NULL then 0 else (case @toDate when NULL then 0 else 1 end) end
+	DECLARE @hasDates BIT = case when @fromDate IS NULL then 0 else (case when @toDate IS NULL then 0 else 1 end) end
 	DECLARE @dynamicSql VARCHAR(MAX) =
-		'select '+case @batchSize when NULL then '' else 'top('+CONVERT(VARCHAR, @batchSize)+')' end +'
+		'select '+case when @batchSize IS NULL then '' else 'top('+CONVERT(VARCHAR, @batchSize)+')' end +'
 			rank() over (order by sj.SCOA_Fund, sj.SCOA_Function, sj.SCOA_Mun_Classification, sj.SCOA_Project, sj.SCOA_Costing, sj.SCOA_Region, sj.SCOA_Item_Debit, sj.SCOA_Fund_Credit, sj.SCOA_Function_Credit, sj.SCOA_Mun_Classification_Credit, sj.SCOA_Project_Credit, sj.SCOA_Costing_Credit, sj.SCOA_Region_Credit, sj.SCOA_Item_Credit) as RollupID,
 			'+CONVERT(VARCHAR, @imqsBatchId)+' as IMQSBatchID,
 			sj.ID
