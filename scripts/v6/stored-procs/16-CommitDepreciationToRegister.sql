f3c255fd43074a176f539469c35f50eb3282d63d
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2017-11-29 15:09:10 +0200

    Fixes CommitDepreciationToRegister

--- scripts/v6/stored-procs/16-CommitDepreciationToRegister.sql
+++ scripts/v6/stored-procs/16-CommitDepreciationToRegister.sql
@@ -8,7 +8,7 @@ BEGIN
 	DECLARE @FinRegister VARCHAR(MAX);
 	SET @FinYear = (SELECT TOP 1 FinYear FROM SCOAJournal WHERE IMQSBatchID = @IMQSBatchID);
 	SET @FinRegister = 'AssetRegisterIconFin' + CAST(@FinYear AS VARCHAR(4));
-	DECLARE @ARSQL = 'UPDATE ar SET
+	DECLARE @ARSQL VARCHAR(MAX) = 'UPDATE ar SET
 						ar.DateLastFinMonth = sj.EffectiveDate,
 						ar.DepreciationFinYTD = ar.DepreciationFinYTD - sj.Amount
 					FROM ' + @FinRegister + '  ar, SCOAJournal sj
@@ -16,7 +16,7 @@ BEGIN
 						ar.ComponentID = sj.ComponentID AND
 						sj.FinancialField = ''DepreciationFinYTD'' AND
 						sj.IMQSBatchID = ' + CONVERT(VARCHAR,@IMQSBatchID);
-	DECLARE @SJSQL = 'UPDATE ScoaJournal SET CommittedToRegister = 1 WHERE IMQSBatchID = '+CONVERT(VARCHAR,@IMQSBatchID);
+	DECLARE @SJSQL VARCHAR(MAX) = 'UPDATE ScoaJournal SET CommittedToRegister = 1 WHERE IMQSBatchID = '+CONVERT(VARCHAR,@IMQSBatchID);
 	EXEC SetFormLevelForImqsBatchId @IMQSBatchID, 3;
 	EXEC(@ARSQL);
 	EXEC(@SJSQL);
