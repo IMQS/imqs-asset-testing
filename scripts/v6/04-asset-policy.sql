e3e555812638245658eb0818949bd24d1c520e7f
Nonzaliseko Phamane <nonzaliseko.phamane@imqs.co.za>
2018-01-30 11:22:26 +0200

    Add Useful Life Measure to AssetPolicyGeneral.

--- scripts/v6/04-asset-policy.sql
+++ scripts/v6/04-asset-policy.sql
@@ -1,8 +1,8 @@
--- Tables relating to the Bill of Quantities (BOQ), referred to the the asset policy in V6
+-- Tables and functions relating to V6 Policy
 
 IF OBJECT_ID (N'AssetPolicyGeneral', N'U') IS NOT NULL DROP TABLE [AssetPolicyGeneral];
+IF OBJECT_ID ('GetForecastReplYear') IS NOT NULL DROP FUNCTION GetForecastReplYear;
 
--- Used by SP GetForecastReplYear that is used by the AssetRegisterIconFin table
 CREATE TABLE [dbo].[AssetPolicyGeneral](
 	[Section] [varchar](40) NOT NULL,
 	[Identifier] [varchar](80) NOT NULL,
@@ -12,3 +12,22 @@ CREATE TABLE [dbo].[AssetPolicyGeneral](
 	[Modified_On] [datetime] NULL,
 	CONSTRAINT [PK_AssetPolicyGeneral_CombKey] PRIMARY KEY CLUSTERED([Section] ASC, [Identifier] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY];
+
+EXECUTE ('CREATE FUNCTION [dbo].[GetForecastReplYear](@RUL NUMERIC(18,2), @FinYear INT)
+  RETURNS INT
+  AS
+  BEGIN
+    DECLARE @Result INT
+    DECLARE @ULM varchar(80)
+
+    SET @ULM = (SELECT UPPER(Value) FROM AssetPolicyGeneral WHERE Identifier = ''Useful Life Measure'');
+
+    IF (@ULM = ''MONTHS'') or (@ULM = ''MONTH'')
+       SET @Result = (CONVERT([int],FLOOR((@FinYear)+@RUL/12),(0)))
+    ELSE IF (@ULM = ''DAYS'') or (@ULM = ''DAY'')
+       SET @Result = (CONVERT([int],FLOOR((@FinYear)+@RUL/365),(0)))
+    ELSE
+       SET @Result = (CONVERT([int],FLOOR((@FinYear)+@RUL),(0)))
+
+    RETURN @Result
+  END;');
