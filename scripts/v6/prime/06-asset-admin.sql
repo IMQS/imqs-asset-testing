0901a86a44726c9d9460f49f890dd079c8eeca11
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2018-02-27 13:55:37 +0200

    Added scoaPostingId counter

--- scripts/v6/prime/06-asset-admin.sql
+++ scripts/v6/prime/06-asset-admin.sql
@@ -1,6 +1,7 @@
 INSERT INTO AssetRefCounter (counter_name, counter_prev_id, counter_current_id, counter_next_id) VALUES ('assetfinformref', 0,1,2);
 INSERT INTO AssetRefCounter (counter_name, counter_prev_id, counter_current_id, counter_next_id) VALUES ('componentidpostfix', 0,1,2);
 INSERT INTO AssetRefCounter (counter_name, counter_prev_id, counter_current_id, counter_next_id) VALUES ('imqsBatchId', NULL,0,1);
+INSERT INTO AssetRefCounter (counter_name, counter_prev_id, counter_current_id, counter_next_id) VALUES ('scoaPostingId', NULL,0,1);
 
 -- AssetFinYear inserts
 INSERT INTO AssetFinYear (FinYear, Period) VALUES(2015,1);
