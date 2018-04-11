a055dacd114247451e2e3f316950a4120b90990b
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2018-04-11 15:15:26 +0200

    V6 Scripts Cleanup; Changed PK on ProjectsSCOASegment to reflect incoming data

--- scripts/v6/stored-procs/01-delete-stored-procs.sql
+++ scripts/v6/stored-procs/01-delete-stored-procs.sql
@@ -3,8 +3,6 @@ IF OBJECT_ID ('ExportSamrasSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE Export
 IF OBJECT_ID ('ExportSamrasSCOAMasterDataForBatch') IS NOT NULL DROP PROCEDURE ExportSamrasSCOAMasterDataForBatch;
 IF OBJECT_ID ('ExportSCOAJournalNoRollup') IS NOT NULL DROP PROCEDURE ExportSCOAJournalNoRollup;
 IF OBJECT_ID ('ExportSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportSCOAJournalRollUp;
-IF OBJECT_ID ('ExportSolarSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportSolarSCOAJournalRollUp;
-IF OBJECT_ID ('ExportVenusSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportVenusSCOAJournalRollUp;
 IF OBJECT_ID ('ExportBcxSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportBcxSCOAJournalRollUp;
 IF OBJECT_ID ('UpdateSCOAJournalSCOAFileName') IS NOT NULL DROP PROCEDURE UpdateSCOAJournalSCOAFileName;
 IF OBJECT_ID (N'SetClassificationSelectable', N'P') IS NOT NULL DROP PROCEDURE [SetClassificationSelectable];
