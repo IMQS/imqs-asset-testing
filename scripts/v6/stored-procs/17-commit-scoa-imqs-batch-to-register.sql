12e14f6ac75c48ceddb9364f17dca00e6195158a
Frank van Riet <frank.van.riet@imqs.co.za>
2018-05-02 17:08:52 +0200

    Removed dead procedures - now handled via input form service

--- scripts/v6/stored-procs/17-commit-scoa-imqs-batch-to-register.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE [dbo].CommitScoaImqsBatchToRegister (
-	@IMQSBatchID BIGINT
-) AS
-BEGIN
-	SET NOCOUNT ON;
-	EXEC SetFormLevelForImqsBatchId @IMQSBatchID, 3;
-	EXEC SetImqsScoaBatchCommitted @IMQSBatchID;
-END
