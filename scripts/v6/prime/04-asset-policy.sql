3e9d58f07df89c223f36d9feee9f880851be0d65
Frank van Riet <frank.van.riet@imqs.co.za>
2018-05-17 09:59:19 +0200

    Added new policy values

--- scripts/v6/prime/04-asset-policy.sql
+++ scripts/v6/prime/04-asset-policy.sql
@@ -4,9 +4,13 @@ INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description
 INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('SCOA', 'SCOA Batch Size', '6', 'The size of SCOA batches', NULL, GETDATE());
 INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('SCOA', 'MunicipalDemarcationCode', 'WC032', 'Unique code by which to identify the municipality, supplied to us by the financial system', NULL, GETDATE());
 INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('SCOA', 'Version', '6.1', 'The version of SCOA definitions used by this municipality', NULL, GETDATE());
+INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('SCOA', 'SCOA Compliant', 'YES', '', NULL, GETDATE());
 
 -- Integration config options
 INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('Integration', 'IntegratedFinSys', 'PUSH', NULL, NULL, GETDATE());
+INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('Integration', 'StatusChangeNotifications', 'NO', NULL, NULL, GETDATE());
+INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('Integration', 'V6CommitToRegister', 'NO', NULL, NULL, GETDATE());
+INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('Integration', 'TransactionFormServiceURL', '', NULL, NULL, GETDATE());
 
 -- General options
 INSERT INTO [AssetPolicyGeneral] ([Section], [Identifier], [Value], [Description], [Modified_By], [Modified_On]) values ('General', 'Useful Life Measure', 'Months', NULL, NULL, GETDATE());
