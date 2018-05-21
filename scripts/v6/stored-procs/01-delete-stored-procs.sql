456012eb415922fd2f398bd543649b045479a224
Frank van Riet <frank.van.riet@imqs.co.za>
2018-05-21 08:02:36 +0200

    Removed dead stuff

--- scripts/v6/stored-procs/01-delete-stored-procs.sql
+++ scripts/v6/stored-procs/01-delete-stored-procs.sql
@@ -1,14 +1,7 @@
 IF OBJECT_ID ('ExportSamrasSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportSamrasSCOAJournalRollUp;
 IF OBJECT_ID ('ExportSamrasSCOAMasterDataForBatch') IS NOT NULL DROP PROCEDURE ExportSamrasSCOAMasterDataForBatch;
-IF OBJECT_ID ('ExportSCOAJournalNoRollup') IS NOT NULL DROP PROCEDURE ExportSCOAJournalNoRollup;
 IF OBJECT_ID ('ExportSCOAJournalRollUp') IS NOT NULL DROP PROCEDURE ExportSCOAJournalRollUp;
 IF OBJECT_ID ('UpdateSCOAJournalSCOAFileName') IS NOT NULL DROP PROCEDURE UpdateSCOAJournalSCOAFileName;
 IF OBJECT_ID (N'SetClassificationSelectable', N'P') IS NOT NULL DROP PROCEDURE [SetClassificationSelectable];
 IF OBJECT_ID (N'NextValueFor', N'P') IS NOT NULL DROP PROCEDURE [NextValueFor];
-IF OBJECT_ID (N'SetFormLevelForImqsBatchId', N'P') IS NOT NULL DROP PROCEDURE [SetFormLevelForImqsBatchId];
-IF OBJECT_ID (N'SetImqsScoaBatchCommitted', N'P') IS NOT NULL DROP PROCEDURE [SetImqsScoaBatchCommitted];
-IF OBJECT_ID (N'DeleteFromScoaDepreciationStatus', N'P') IS NOT NULL DROP PROCEDURE [DeleteFromScoaDepreciationStatus];
-IF OBJECT_ID(N'[dbo].[CommitDepreciationToRegister]', N'P') IS NOT NULL DROP PROCEDURE [dbo].CommitDepreciationToRegister;
-IF OBJECT_ID (N'CommitScoaImqsBatchToRegister', N'P') IS NOT NULL DROP PROCEDURE [CommitScoaImqsBatchToRegister];
-IF OBJECT_ID (N'GetFinancialSystemWorkflowType', N'P') IS NOT NULL DROP PROCEDURE [GetFinancialSystemWorkflowType];
 IF OBJECT_ID (N'SetValueFor', N'P') IS NOT NULL DROP PROCEDURE [SetValueFor];
