c7d7f80721cffa3e631dabfb514d605ec61e08a9
Nonzaliseko Phamane <nonzaliseko.phamane@imqs.co.za>
2018-10-03 16:46:44 +0200

    Create and prime SCOASettings table. TA8-1664

--- scripts/v6/prime/07-asset-scoa.sql
+++ scripts/v6/prime/07-asset-scoa.sql
@@ -13,3 +13,66 @@ INSERT INTO SCOABudgetLeg ([FinFormNr], [TransactionType], [FinancialField], [Bu
 INSERT INTO SCOABudgetLeg ([FinFormNr], [TransactionType], [FinancialField], [BudgetLeg]) VALUES (11,'Revaluation','RevaluationReserveFinYTDImp','DT');
 INSERT INTO SCOABudgetLeg ([FinFormNr], [TransactionType], [FinancialField], [BudgetLeg]) VALUES (7,'ValueChange','ValueChangeFinYTD','DT');
 INSERT INTO SCOABudgetLeg ([FinFormNr], [TransactionType], [FinancialField], [BudgetLeg]) VALUES (12,'Maintenance','MaintenanceExpenditure','DT');
+
+
+--Get the current Financial Year
+DECLARE @FinYear INT = (SELECT MAX(FinYear) FROM AssetFinYear);
+
+IF (@FinYear IS NULL)
+BEGIN
+	SET @FinYear = (SELECT
+					CASE
+						WHEN (SELECT MONTH(GETDATE())) <= 6 THEN (SELECT YEAR(GETDATE())) ELSE (SELECT YEAR(GETDATE())) + 1
+					END)
+END;
+
+
+--Add the defaults
+IF NOT EXISTS (SELECT * FROM SCOASettings WHERE Identifier = 'DEFAULT_SCOA_FUND' AND FinYear = @FinYear)
+BEGIN
+	INSERT INTO SCOASettings (FinYear, Identifier, Value, "Description")
+	VALUES (@FinYear,'DEFAULT_SCOA_FUND','5692f970-c29c-4044-80d7-1bcdbdd34348','Default SCOAId to be used for SCOA_FUND')
+END;
+
+IF NOT EXISTS (SELECT * FROM SCOASettings WHERE Identifier = 'DEFAULT_SCOA_PROJECT' AND FinYear = @FinYear)
+BEGIN
+	INSERT INTO SCOASettings (FinYear, Identifier, Value, "Description")
+	VALUES (@FinYear,'DEFAULT_SCOA_PROJECT','2a79a136-fcdf-43bf-8c4e-8ed735d13905','Default SCOAId to be used for SCOA_PROJECT')
+END;
+
+IF NOT EXISTS (SELECT * FROM SCOASettings WHERE Identifier = 'DEFAULT_SCOA_COST' AND FinYear = @FinYear)
+BEGIN
+	INSERT INTO SCOASettings (FinYear, Identifier, Value, "Description")
+	VALUES (@FinYear,'DEFAULT_SCOA_COST','47c7ba65-c270-4a7f-91ba-3842eb629ddf','Default SCOAId to be used for SCOA_COST')
+END;
+
+--REGION is client specific and must be set through IMQS SCOA setup
+IF NOT EXISTS (SELECT * FROM SCOASettings WHERE Identifier = 'DEFAULT_SCOA_REGION' AND FinYear = @FinYear)
+BEGIN
+	INSERT INTO SCOASettings (FinYear, Identifier, Value, "Description")
+	VALUES (@FinYear,'DEFAULT_SCOA_REGION','NO DEFAULT SET','Default SCOAId to be used for SCOA_REGION')
+END;
+
+IF NOT EXISTS (SELECT * FROM SCOASettings WHERE Identifier = 'WIPFROM_SCOA_ITEM' AND FinYear = @FinYear)
+BEGIN
+	INSERT INTO SCOASettings (FinYear, Identifier, Value, "Description")
+	VALUES (@FinYear,'WIPFROM_SCOA_ITEM','NO DEFAULT SET','Default SCOAId to be used for WIP-From SCOA Item on rollover')
+END;
+
+IF NOT EXISTS (SELECT * FROM SCOASettings WHERE Identifier = 'WIPTO_SCOA_ITEM' AND FinYear = @FinYear)
+BEGIN
+	INSERT INTO SCOASettings (FinYear, Identifier, Value, "Description")
+	VALUES (@FinYear,'WIPTO_SCOA_ITEM','NO DEFAULT SET','Default SCOAId to be used for WIP-To SCOA Item on rollover')
+END;
+
+
+--Delete old values from AssetPolicyGeneral
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'DEFAULT_SCOA_FUND';
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'DEFAULT_SCOA_PROJECT';
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'DEFAULT_SCOA_COST';
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'DEFAULT_SCOA_REGION';
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'OpenBalanceTransferItem';
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'SCOA Journal Frequency';
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'SCOA Journal Location';
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'SCOA Journal Roll-up';
+DELETE FROM AssetPolicyGeneral WHERE Section = 'SCOA' and Identifier = 'Version';
