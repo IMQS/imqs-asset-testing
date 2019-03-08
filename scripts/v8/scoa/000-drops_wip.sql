6c46343683ebecb870057917c32d7fb4a2c8ee88
Donovan Thatcher <donovan.thatcher@imqs.co.za>
2019-03-08 13:48:05 +0200

    Using truncate to empty pg tables

--- scripts/v8/scoa/000-drops_wip.sql
+++ scripts/v8/scoa/000-drops_wip.sql
@@ -7,23 +7,34 @@ END;
 $$
 LANGUAGE plpgsql;
 
-select purge('CapitalisationGroupExpenditure');
-select purge('CapitalisationGroup');
-select purge('ComponentTransaction');
-select purge('ComponentReplacement');
-select purge('ComponentOwnership');
-select purge('ComponentLocation');
-select purge('ComponentLifeCycleIndicators');
-select purge('ComponentDerecognition');
-select purge('ComponentBomPolicy');
-select purge('ComponentDescription');
-select purge('ActualFinancials');
-select purge('ActualDetails');
-select purge('ActualIdentification');
-select purge('BudgetApportions');
-select purge('ProjectBudgetLink');
-select purge('ProjectFinancials');
-select purge('ProjectWorkflow');
-select purge('ProjectGeneral');
-select purge('BudgetSCOA');
-select purge('Budget');
+create or replace function truncate_if_exists(tablename text)
+	returns void language plpgsql as $$
+begin
+	perform 1
+	from information_schema.tables
+	where table_name = tablename;
+	if found then
+		execute format('truncate %I cascade;', tablename);
+	end if;
+end $$;
+
+select truncate_if_exists('CapitalisationGroupExpenditure');
+select truncate_if_exists('CapitalisationGroup');
+select truncate_if_exists('ComponentTransaction');
+select truncate_if_exists('ComponentReplacement');
+select truncate_if_exists('ComponentOwnership');
+select truncate_if_exists('ComponentLocation');
+select truncate_if_exists('ComponentLifeCycleIndicators');
+select truncate_if_exists('ComponentDerecognition');
+select truncate_if_exists('ComponentBomPolicy');
+select truncate_if_exists('ComponentDescription');
+select truncate_if_exists('ActualFinancials');
+select truncate_if_exists('ActualDetails');
+select truncate_if_exists('ActualIdentification');
+select truncate_if_exists('BudgetApportions');
+select truncate_if_exists('ProjectBudgetLink');
+select truncate_if_exists('ProjectFinancials');
+select truncate_if_exists('ProjectWorkflow');
+select truncate_if_exists('ProjectGeneral');
+select truncate_if_exists('BudgetSCOA');
+select truncate_if_exists('Budget');
