ae3732d7ce6bef5ce18e6638df25856f20695fa8
Nonzaliseko Phamane <nonzaliseko.phamane@imqs.co.za>
2019-07-11 11:07:34 +0200

    Delete view

--- scripts/v6/views/00-delete-views.sql
+++ scripts/v6/views/00-delete-views.sql
@@ -1,1 +1,4 @@
 IF OBJECT_ID (N'AssetRegisterView', N'V') IS NOT NULL DROP View [AssetRegisterView];
+IF OBJECT_ID (N'AssetRegisterView2014', N'V') IS NOT NULL DROP View [AssetRegisterView2014];
+IF OBJECT_ID (N'AssetRegisterView2015', N'V') IS NOT NULL DROP View [AssetRegisterView2015];
+IF OBJECT_ID (N'AssetRegisterView2017', N'V') IS NOT NULL DROP View [AssetRegisterView2017];
