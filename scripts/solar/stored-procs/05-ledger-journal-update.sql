69d616fec4d930460529f2ad190e3fc5ec3f7a91
Donovan Thatcher <DonovanT@imqs.local>
2020-02-13 12:15:06 +0200

    TA8-2193 - updates to Solar Stored Procs

--- scripts/solar/stored-procs/05-ledger-journal-update.sql
+++ scripts/solar/stored-procs/05-ledger-journal-update.sql
@@ -1,5 +1,5 @@
-CREATE PROCEDURE [dbo].[P_F_G_UPD_LEDGER_JOURNAL] (@ttLedgerJournal TT_F_G_LEDGER_JOURNAL READONLY , @chvVendorCode Varchar(2),
-@chvReturnStatus VARCHAR(50) OUTPUT, @intBatchNumber BIGINT OUTPUT, @intRecordsReceived INT OUTPUT, @intRecordsProcessed INT OUTPUT, @intRecordsFailed INT OUTPUT)
+CREATE PROCEDURE P_F_G_UPD_LEDGER_JOURNAL (@ttLedgerJournal TT_F_G_LEDGER_JOURNAL READONLY , @chvVendorCode Varchar(2), @bitTestRun BIT, 
+@chvReturnStatus VARCHAR(50) OUTPUT, @intBatchNumber BIGINT OUTPUT, @intRecordsReceived INT OUTPUT, @intRecordsProcessed INT OUTPUT, @intRecordsFailed INT OUTPUT) 
 AS
 BEGIN
 	SELECT @intRecordsReceived = COUNT(*) FROM @ttLedgerJournal
@@ -7,11 +7,18 @@ BEGIN
 	SET @intRecordsFailed = 0
 	SET @intBatchNumber = (SELECT CASE WHEN MAX(BATCH_NUMBER) IS NULL THEN 1 ELSE MAX(BATCH_NUMBER)+1 END FROM LedgerJournalSummary)
 	SET @chvReturnStatus = 'PASS'
-
 	DECLARE @currDateTime datetime2 = (SELECT GETDATE())
-
-	INSERT INTO LedgerJournalSummary(BATCH_NUMBER,VENDOR_CODE,BATCH_DATE,RECORDS_RECEIVED,RECORDS_VALIDATED,RECORDS_FAILED,RECORDS_UNAUTHORISED,RECORDS_AUTHORISED,RECORDS_AUTHORISATION_FAILED)
-	VALUES (@intBatchNumber, '04', @currDateTime, @intRecordsReceived, @intRecordsReceived, 0, 0,  @intRecordsReceived, 0);
-	
-	INSERT INTO LedgerJournal SELECT * FROM @ttLedgerJournal
-END;
+	BEGIN TRAN
+		INSERT INTO LedgerJournalSummary(BATCH_NUMBER,VENDOR_CODE,BATCH_DATE,RECORDS_RECEIVED,RECORDS_VALIDATED,RECORDS_FAILED,RECORDS_UNAUTHORISED,RECORDS_AUTHORISED,RECORDS_AUTHORISATION_FAILED)
+		VALUES (@intBatchNumber, '04', @currDateTime, @intRecordsReceived, @intRecordsReceived, 0, 0, 0, 0)	
+		INSERT INTO LedgerJournal SELECT * FROM @ttLedgerJournal
+		IF @bitTestRun != 1 
+		BEGIN
+			COMMIT
+		END
+		ELSE 
+		BEGIN
+			PRINT 'Test run detected - after successfully creating journals, the MOCK BCX database will now roll back the changes made'
+			ROLLBACK
+		END
+END
